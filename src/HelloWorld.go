28efb4089b81856b3da35e1b81ceb0b9233a190a
Yuri <yurifullstack@gmail.com>
2021-01-10 14:35:41 -0300

    for statement

--- src/HelloWorld.go
+++ src/HelloWorld.go
@@ -1,8 +1,19 @@
 package main
 
-import "fmt"
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
+	var s, sep string
 	fmt.Println(os.Args)
+	fmt.Println(os.Args[1])
+	fmt.Println(len(os.Args))
+
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
 }
