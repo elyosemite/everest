8af79912a8b9ad47ae520a8e82c0b86bf37670fe
Yuri <yurifullstack@gmail.com>
2021-01-10 14:53:55 -0300

    range in loops

--- src/Loops.go
+++ src/Loops.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 	i := 0
+	rvariable := []string{"Yuri Melo", "Pedro Da Silva", "Kim Jong un"}
 
 	for i := 0; i < 4; i++ {
 		fmt.Println("I'm back-end Developer")
@@ -14,7 +15,13 @@ func main() {
 	}
 	fmt.Println(i)
 
+	// INfinity loop
 	/*for {
 		fmt.Println("I am infinity loop")
 	}*/
+
+	for i, j := range rvariable {
+		fmt.Println(i, j)
+	}
+
 }
