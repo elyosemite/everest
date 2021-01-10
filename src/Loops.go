2f1ee5cd711d0c256c0a3dd76d0896abf6411f37
Yuri <yurifullstack@gmail.com>
2021-01-10 14:59:03 -0300

    loop in map

--- src/Loops.go
+++ src/Loops.go
@@ -5,6 +5,11 @@ import "fmt"
 func main() {
 	i := 0
 	rvariable := []string{"Yuri Melo", "Pedro Da Silva", "Kim Jong un"}
+	mmap := map[int]string {
+		22: "Yuri Melo",
+		33: "GFG",
+		44: "Igor Melo",
+	}
 
 	for i := 0; i < 4; i++ {
 		fmt.Println("I'm back-end Developer")
@@ -22,6 +27,15 @@ func main() {
 
 	for i, j := range rvariable {
 		fmt.Println(i, j)
+	}
+
+	for i, j := range "Yuri dos Santos Melo" {
+		fmt.Printf("The index number of %U is %d\n", j, i)
+	}
+
+	// Loop in maps
+	for key, value := range mmap {
+		fmt.Println(key, value)
 	}
 
 }
