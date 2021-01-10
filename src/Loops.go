79d5d5f6012e41c7393604d9b7e3f85aea928bd1
Yuri <yurifullstack@gmail.com>
2021-01-10 14:52:17 -0300

    for loop

--- src/Loops.go
+++ src/Loops.go
@@ -3,9 +3,16 @@ package main
 import "fmt"
 
 func main() {
+	i := 0
+
 	for i := 0; i < 4; i++ {
 		fmt.Println("I'm back-end Developer")
 	}
+
+	for i < 3 {
+		i += 2
+	}
+	fmt.Println(i)
 
 	/*for {
 		fmt.Println("I am infinity loop")
