3840d91bb096e9c9089372057bc929a5d3279883
Yuri Melo <yurifullstack@gmail.com>
2021-08-28 22:16:34 -0300

    Data types for Go Language

---
+++ src/datatypes.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Printing varaible value")
+
+	a := 1
+	b := "Yuri Melo"
+	c := true
+	d = `Yuri
+	dos
+	Santos
+	Melo`
+	e := 3.1415
+
+	fmt.Printf("%v\n", a)
+	fmt.Printf("%v\n", b)
+	fmt.Printf("%v\n", c)
+	fmt.Printf("%v\n", d)
+	fmt.Printf("%v\n", e)
+
+}
