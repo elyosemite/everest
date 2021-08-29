61caaa0a933b1427e2b43bc4b3017a3e18d5841c
Yuri Melo <yurifullstack@gmail.com>
2021-08-29 11:41:23 -0300

    Array

---
+++ src/array.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	// The capacity of an array is defined at creation time.
+	
+	coral := [3]string{"blue coral", "staghorn coral", "pillar coral"}
+	fmt.Println(coral)
+}
