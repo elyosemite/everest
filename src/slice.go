168a90b783e5877fc455fa483428b7923fe5e73f
Yuri Melo <yurifullstack@gmail.com>
2021-08-29 18:33:50 -0300

    Study slicecs

---
+++ src/slice.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// My slice type
+	// []int{-3, -2, -1, 0, 1, 2, 3}
+	seaCreatures := []string{ "shark", "cuttlefish", "squid", "mantis shrimp"}
+	fmt.Println(seaCreatures)
+
+	seaCreatures = append(seaCreatures, "seahourse")
+
+	fmt.Println(seaCreatures)
+}
