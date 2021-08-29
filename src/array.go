1015c25551ebf312e021778da663f15559d2a8e7
Yuri Melo <yurifullstack@gmail.com>
2021-08-29 18:16:01 -0300

    Add prime numbers

--- src/array.go
+++ src/array.go
@@ -4,7 +4,9 @@ import "fmt"
 
 func main() {
 	// The capacity of an array is defined at creation time.
-	
+
 	coral := [3]string{"blue coral", "staghorn coral", "pillar coral"}
+	primes := [6]int{2, 3, 5, 7, 11, 13}
 	fmt.Println(coral)
+	fmt.Println(primes)
 }
