97f315930e43d52ae2681602c894343451272bfb
Yuri Melo <yurifullstack@gmail.com>
2023-04-29 13:36:38 -0300

    Studying constants

---
+++ src/fundamentals/constants.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+const age = 25
+const name string = "Yuri Dos Santos Melo" 
+
+var my_age_typed int
+
+func main() {
+	my_age_typed = age
+
+	fmt.Println(my_age_typed)
+}
