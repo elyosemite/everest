50a6ecff2abe4053cf1a8d0a62dfcc2578eb1aa9
Yuri Melo <yurifullstack@gmail.com>
2026-05-10 15:56:11 -0300

    Add AccountType definition and String method for account types

---
+++ internal/domain/account/account_type.go
@@ -0,0 +1,19 @@
+package account
+
+type AccountType int
+
+const (
+	Checking AccountType = iota
+	Savings
+)
+
+func (t AccountType) String() string {
+	switch t {
+	case Checking:
+		return "Conta Corrente"
+	case Savings:
+		return "Conta Poupança"
+	default:
+		return "Conta desconhecida"
+	}
+}
