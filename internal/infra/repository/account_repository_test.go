8cd1f2bfae1d6eb7a1fa6112f1d50844302db72c
Yuri Melo <yurifullstack@gmail.com>
2026-05-12 20:08:50 -0300

    test: check out if properties in object correctly return

--- internal/infra/repository/account_repository_test.go
+++ internal/infra/repository/account_repository_test.go
@@ -40,13 +40,11 @@ func TestInMemoryAccountRepository_FindByID_NotFound(t *testing.T) {
 	}
 }
 
-func TestInMemoryAccountRepository_FindByUserID(t *testing.T) {
+func TestInMemoryAccountRepository_FindAccountByUserID(t *testing.T) {
 	repo := repository.NewInMemoryAccountRepository()
 
 	a1, _ := account.NewAccount("user-1", account.Checking, 100)
-
 	a2, _ := account.NewAccount("user-1", account.Savings, 200)
-
 	a3, _ := account.NewAccount("user-2", account.Checking, 50)
 
 	repo.Save(a1)
@@ -83,5 +81,26 @@ func TestInmemoryRepository_TrySaveExistingAccount(t *testing.T) {
 	err := repo.Save(newAccount)
 	if err == nil {
 		t.Fatal("expected error when saving duplicate account, got nil")
+	}
+}
+
+func TestInMemoryAccountRepository_FindByID_ReturnsCorrectData(t *testing.T) {
+	repo := repository.NewInMemoryAccountRepository()
+
+	a, _ := account.NewAccount("user-1", account.Checking, 250.0)
+	repo.Save(a)
+
+	found, err := repo.FindByID(a.ID())
+	if err != nil {
+		t.Fatalf("expected no error, got: %v", err)
+	}
+	if found.ID() != a.ID() {
+		t.Fatalf("ID: got %q, want %q", found.ID(), a.ID())
+	}
+	if found.Type() != a.Type() {
+		t.Errorf("Type: got %v, want %v", found.Type(), account.Savings)
+	}
+	if found.Balance() != 250.0 {
+		t.Errorf("Balance: got %v, want %v", found.Balance(), 250.0)
 	}
 }
